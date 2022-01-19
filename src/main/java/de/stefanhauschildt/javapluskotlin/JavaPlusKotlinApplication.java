c94c566c75b4d63d6eaa09ac3d01fa638ca5f614
Stefan Hauschildt <stefan.h@uschildt.de>
2022-01-19 12:56:08 +0100

    initial commit

---
+++ src/main/java/de/stefanhauschildt/javapluskotlin/JavaPlusKotlinApplication.java
@@ -0,0 +1,13 @@
+package de.stefanhauschildt.javapluskotlin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaPlusKotlinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaPlusKotlinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/stefanhauschildt/javapluskotlin/JavaPlusKotlinApplicationTests.java
@@ -0,0 +1,13 @@
+package de.stefanhauschildt.javapluskotlin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaPlusKotlinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
